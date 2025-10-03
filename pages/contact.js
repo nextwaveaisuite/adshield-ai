2dff84213c57bd3f91f7f34b7c49c4cb24642a1a
nextwaveaisuite <nextwaveaisuite@gmail.com>
2025-10-03 18:31:18 +1000

    Delete pages directory

--- pages/contact.js
+++
@@ -1,1 +0,0 @@
-export default function Page(){return(<main style={{padding:'24px'}}><h1>Contact</h1></main>);}
--- pages/disclaimer.js
+++
@@ -1,1 +0,0 @@
-export default function Page(){return(<main style={{padding:'24px'}}><h1>Disclaimer</h1></main>);}
